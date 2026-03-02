e7f3e731e4af0d71edfb85d760cfa1e6fd911538
msaundra <156338804+msaundra@users.noreply.github.com>
2026-03-02 11:57:22 -0700

    changes: caoursel component, index blocks, etc.

---
+++ frontend/src/components/ui/EmblaCarousel.jsx
@@ -0,0 +1,31 @@
+// src/components/EmblaCarousel.jsx
+import React, { useEffect } from 'react';
+import Autoplay from 'embla-carousel-autoplay';
+import useEmblaCarousel from 'embla-carousel-react';
+
+const EmblaCarousel = ({ slides, options }) => {
+  const [emblaRef, emblaApi] = useEmblaCarousel(options, [Autoplay()]);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+    const autoplay = emblaApi.plugins()?.autoplay;
+    if (!autoplay) return;
+    autoplay.play();
+  }, [emblaApi]);
+
+  return (
+    <section className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {slides.map((slide, index) => (
+            <div className="embla__slide" key={index}>
+              <img src={slide.img} alt={slide.title} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default EmblaCarousel;
---
+++ frontend/src/components/ui/HoursTab.jsx
@@ -0,0 +1,1 @@
+//this will be the place where I create our hours tab
