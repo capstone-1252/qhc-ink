23485f55cf40406690753bc171361783bf15d1bb
msaundra <156338804+msaundra@users.noreply.github.com>
2026-03-28 12:54:34 -0600

    food bank

--- backend/src/api/food-bank-time-slot/services/food-bank-time-slot.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * food-bank-time-slot service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::food-bank-time-slot.food-bank-time-slot');
